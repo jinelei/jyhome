98c99e1d2a19f0cbcbd7f2fea92b50ef51c1ea02
jinelei <jinelei@163.com>
2019-07-19 17:41:49 +0000

    add: user_frament

--- app/src/main/java/cn/jinelei/jyhome/page/user/UserFragment.java
+++ app/src/main/java/cn/jinelei/jyhome/page/user/UserFragment.java
@@ -1,14 +1,10 @@
 package cn.jinelei.jyhome.page.user;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import cn.jinelei.jyhome.R;
@@ -21,7 +17,7 @@ public class UserFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
     @Override
